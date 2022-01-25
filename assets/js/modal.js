017b299341324e59581e0a0ce820888b8b05d669
Nicolas Moises <nicolas.nicc21@gmail.com>
2022-01-25 15:05:38 -0300

    first commit

---
+++ assets/js/modal.js
@@ -0,0 +1,19 @@
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close');
+
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () => {
+        modal(i)
+    })
+})
+modalClose.forEach((mc) => {
+    mc.addEventListener('click', () => {
+        modalViews.forEach((mv) => {
+            mv.classList.remove('active-modal')
+        })
+    })
+})
